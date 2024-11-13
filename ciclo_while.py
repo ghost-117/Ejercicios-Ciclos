6ae624a2c387b1dd8eae13f7fbf86921ecc28f56
ghost-117 <krono117pro1@gmail.com>
2024-11-12 18:27:05 -0600

    Ejemplo de ciclo

---
+++ ciclo_while.py
@@ -0,0 +1,30 @@
+"""
+Ciclo while
+while ep_ booleana:
+ instrucciones 
+ actualizacion de valores 
+"""
+#num = 1
+#while num < 10:
+ #   print( f"Hello {num} ")
+  #  num = num + 5
+    
+# Mas operadores matematicos
+""""  operadores de asignacion 
+n = n + 5 == n +=5
+n = n - 5 == n -=5
+n = n * 5 == n *=5
+n = n / 5 == n /=5
+"""    
+
+"""
+Implementacion Dp While (hacer , hasta) en Python
+ While True:
+ instrucciones 
+ if exp_ bool:
+   break
+"""
+while True:
+    option = input(" Escribir Salir: " )
+    if option == "Salir":
+       break 
