d8d7c0b58e85b44184db6994a703a2285294b6f2
ghost-117 <krono117pro1@gmail.com>
2024-11-12 20:22:02 -0600

    Ejercicio 01 de ciclos

--- ejercicio01.py
+++ ejercicio01.py
@@ -8,4 +8,4 @@ num = int(input("Ingresa un número: "))
 factorial = 1
 for i in range(1, num + 1):
     factorial *= i
-print(f"El factorial de {num} es {factorial}") 
+print(f"El factorial del numero {num} es {factorial}") 
